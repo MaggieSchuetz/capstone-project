1602e8694fcf73141231d26d13eef760ca7e6614
Magdalena Schuetz <m.schuetz@outlook.de>
2022-03-09 15:34:54 +0100

    Add function delete entry with confirmation window

--- src/components/JournalEntry.js
+++ src/components/JournalEntry.js
@@ -1,28 +1,30 @@
 import React from 'react';
 import Card from './shared/Card';
 import styled from 'styled-components';
+import { FaTrashAlt } from 'react-icons/fa';
 
-function JournalEntry({ item }) {
+function JournalEntry({ item, handleDelete }) {
   return (
     <Card>
-      <div className="text-display">
-        <Date>{item.date}</Date>
-        <h2>{item.title}</h2>
-        <p>{item.text}</p>
-      </div>
+      <Date>{item.date}</Date>
+      <h2>{item.title}</h2>
+      <p>{item.text}</p>
+      <button
+        onClick={() => {
+          handleDelete(item.id);
+        }}
+      >
+        <FaTrashAlt />
+      </button>
     </Card>
   );
 }
 
-const StyledDate = styled.p`
+const Date = styled.p`
   color: grey;
   position: absolute;
   top: 10px;
   right: 20px;
 `;
-
-function Date({ children }) {
-  return <StyledDate>{children}</StyledDate>;
-}
 
 export default JournalEntry;
