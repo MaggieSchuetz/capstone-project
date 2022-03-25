c92a5741b62af67e62c7f8115119a233b879e963
Magdalena Schuetz <m.schuetz@outlook.de>
2022-03-25 14:59:04 +0100

    Test push to look at form on phone

--- src/components/Form.js
+++ src/components/Form.js
@@ -269,7 +269,7 @@ const FormContainer = styled.form`
   color: #333;
   border-radius: 15px;
   padding: 40px 60px;
-  margin: 20px 20px 100px 20px;
+  margin: 20px 20px 200px 20px;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 `;
 
